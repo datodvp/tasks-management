18968ac11d1c36936cb3440fc84d4eff49fa7889
datodvp <gaguadato98@gmail.com>
2023-10-27 04:52:40 +0400

    feat: basic home page

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,5 +1,64 @@
-<script setup></script>
+<script setup>
+import { ref, watch } from "vue";
+import { router, Link, useForm } from "@inertiajs/vue3";
+
+const props = defineProps({
+    user: String,
+    data: Object,
+});
+
+let form = useForm({
+    firstname: "",
+    lastname: "",
+});
+
+const date = ref(props.data?.filters?.date);
+
+watch(date, (value) => {
+    router.get("/", { date: value });
+});
+
+const submit = () => {
+    form.post("/employees");
+    form.reset();
+};
+</script>
 
 <template>
-    <h1>Welcome</h1>
+    <div>
+        <div>
+            <Link href="/">
+                <h1>Reset {{ user }}</h1>
+            </Link>
+
+            <input v-model="date" type="date" />
+
+            <h1 v-if="data">
+                Selected week: {{ data.startDate }} - {{ data.endDate }}
+            </h1>
+        </div>
+        <div>
+            <form @submit.prevent="submit">
+                <h1>დაამატე ახალი თანამშრომელი:</h1>
+                <div class="flex flex-col">
+                    <input
+                        v-model="form.firstname"
+                        type="text"
+                        placeholder="სახელი"
+                        class="p-2 border border-black"
+                    />
+                    <p class="text-red-500">{{ form.errors.firstname }}</p>
+                    <input
+                        v-model="form.lastname"
+                        type="text"
+                        placeholder="გვარი"
+                        class="p-2 border border-black"
+                    />
+                    <p class="text-red-500">{{ form.errors.lastname }}</p>
+                </div>
+
+                <button class="p-2 border border-black">დამატება</button>
+            </form>
+        </div>
+    </div>
 </template>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Models\Employee;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+
+use function Laravel\Prompts\search;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +19,33 @@ use Inertia\Inertia;
 |
 */
 
-Route::get('/', function () {
-    return Inertia::render('Home');
+Route::get('/', function (Request $request) {
+    $selectedDate = $request->query('date')
+        ? Carbon::parse($request->input('date'))
+        : Carbon::now();
+
+    $weekNumber = $selectedDate->weekOfYear;
+    $weekStartDate = $selectedDate->startOfWeek()->format('d/m');
+    $weekEndDate = $selectedDate->endOfWeek()->format('d/m');
+
+    $data = [
+        'weekNumber' => $weekNumber,
+        'startDate' => $weekStartDate,
+        'endDate' => $weekEndDate,
+    ];
+
+    if ($request->query('date')) {
+        $data['filters'] = ['date' => $request->query('date')];
+    }
+
+    return Inertia::render('Home', ['data' => $data]);
+})->name('home');
+
+Route::post('/employees', function (Request $request) {
+    $attributes = $request->validate([
+        'firstname' => 'required|string|min:2|max:30',
+        'lastname' => 'required|string|min:2|max:30'
+    ]);
+
+    Employee::create($attributes);
 });
